7111a6ff83ba5b806be479621a03d22ccf9c1b16
shalom-cyberlab <oh.shalom.0@gmail.com>
2025-03-01 11:13:26 +0000

    varibles

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+some_name="Hello World"  # assigning variables
+
+$some_name # reference variables
+
+readonly new_var # readonly or immutable variable - constant
+export VAR="value"  # make accessible in subshells and scripts executed from the current shell
